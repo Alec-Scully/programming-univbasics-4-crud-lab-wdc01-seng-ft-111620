a8f69dbcc1b76e999e66087d2be3dabb9b50b63d
Alec Scully <scullyalec@gmail.com>
2020-08-27 00:07:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -44,5 +44,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  
+  new_array =["wow", "I", "am", "really", "learning", "arrays!"]
+  new_array[4] == "totally"
 end
